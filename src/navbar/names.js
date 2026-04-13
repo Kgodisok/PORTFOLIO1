eb66ba844c574c3b230a425c27ec4e928fff249c
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 14:24:07 +0200

    Update Information Details

--- src/navbar/names.js
+++ src/navbar/names.js
@@ -50,6 +50,7 @@ const NavBar = () => {
         <h2>Aspiring Software Developer</h2>
         <p>I'm a passionate and dedicated software developer with a strong desire to create innovative solutions. With a solid foundation in programming languages such as JavaScript, Python, and Java, I have experience in developing web applications, mobile apps, and software solutions. I am constantly learning and staying up-to-date with the latest technologies to enhance my skills and deliver high-quality work. I am eager to contribute my knowledge and creativity to projects that make a positive impact.</p>  
         <p>Welcome to my portfolio</p>
+        <p>I am {`${data.age} `} Years Old</p>
         <img src='/images/image.png' alt={`${data.name} ${data.surname}`} style={imageStyles}/>
         </>
     )
