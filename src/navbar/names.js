9d324eaadc5692ff3fff970d6a6b8fd09f33783a
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-13 14:27:02 +0200

    Change Case

--- src/navbar/names.js
+++ src/navbar/names.js
@@ -46,7 +46,7 @@ const NavBar = () => {
     if (!data) return <p>Loading...</p>;
     return (
         <>
-        <h1 style={styles}>Hello there, I am {`${data.name} ${data.surname}`}</h1>
+        <h1 style={styles}>Hello there, I am {`${data.name.toLocaleLowerCase()} ${data.surname}`}</h1>
         <h2>Aspiring Software Developer</h2>
         <p>I'm a passionate and dedicated software developer with a strong desire to create innovative solutions. With a solid foundation in programming languages such as JavaScript, Python, and Java, I have experience in developing web applications, mobile apps, and software solutions. I am constantly learning and staying up-to-date with the latest technologies to enhance my skills and deliver high-quality work. I am eager to contribute my knowledge and creativity to projects that make a positive impact.</p>  
         <p>Welcome to my portfolio</p>
