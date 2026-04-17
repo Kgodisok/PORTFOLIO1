321ce3d2c1b42a6bd1ebc031771c46e2e9f7e362
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-17 13:11:17 +0200

    Update Styles

--- src/navbar/footer.js
+++ src/navbar/footer.js
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 import localData from "../data/data.json";
 import { FaWhatsapp, FaEnvelope, FaPhone, FaLinkedinIn } from "react-icons/fa";
+import { BiColor } from "react-icons/bi";
+
+const iconStyles ={
+    display: 'flex',
+    gap: '8px',
+    justifyContent: 'center',
+    color: 'grey',
+    backgroundColor: 'black',
+    fontSize: '25px',
+}
 
 const Footer = () => {
     const [data, setData] = useState(localData || null);
     
         useEffect(() => {
-            fetch("/information/data.json")
+            fetch("../data/data.json")
             .then((res) => {
                 if (!res.ok) {
                 throw new Error("Failed to fetch JSON");
@@ -22,23 +32,26 @@ const Footer = () => {
     
         if (!data) return <p>Loading...</p>;
     return (
-        <footer>
-            <p>&copy; 2026 Matsepe Kgodiso, <em>Built In React.js. Portfolio in Progress.</em></p>
-            <FaWhatsapp />
-            <a href={`https://wa.me/${data.cellPhoneNumber[0]}`} target="_blank" rel="noopener noreferrer">WhatsApp Me</a>
-            <br />
-            <FaEnvelope />
-            <a href={`mailto:${data.emailAddress}`}>Email Me</a>
-            <br />
-            <FaPhone />
-            <a href={`tel:${data.cellPhoneNumber[0]}`}>
-            Call Me</a>
-            <br />
-            <a href={data.linkedIn} target="_blank" rel="noopener noreferrer">
-            <FaLinkedinIn />
-            LinkedIn
-            </a>
-        </footer>
+    
+            <footer styles={{backgroundColor: 'black', marginBottom: '0'}}>
+                <p>&copy; 2026 Matsepe Kgodiso, <em>Built In React.js. Portfolio in Progress.</em></p>
+                <div style={iconStyles}>
+                    <FaWhatsapp />
+                    <a href={`https://wa.me/${data.cellPhoneNumber[0]}`} target="_blank" rel="noopener noreferrer">WhatsApp Me</a>
+                    <br />
+                    <FaEnvelope />
+                    <a href={`mailto:${data.emailAddress}`}>Email Me</a>
+                    <br />
+                    <FaPhone />
+                    <a href={`tel:${data.cellPhoneNumber[0]}`}>
+                    Call Me</a>
+                    <br />
+                    <a href={data.linkedIn} target="_blank" rel="noopener noreferrer">
+                    <FaLinkedinIn />
+                    LinkedIn
+                    </a>
+                    </div>
+            </footer>
     );
 };
 
--- src/navbar/names.js
+++ src/navbar/names.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import localData from "../data/data.json";
 
 
-const styles = {
+const nameStyles = {
     fontSize: '45px',
-    color: 'blue',
+    color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: '20px'
@@ -17,9 +17,13 @@ const imageStyles = {
     margin: '20px auto'
 };
 
+const navaBarStyles = {
+    backgroundColor: 'black'
+};
+
 const descriptionStyles = {
     fontSize: '25px',
-    color: '#333',
+    color: 'grey',
     lineHeight: '1.6'
 };
 
@@ -43,14 +47,14 @@ const NavBar = () => {
 
     if (!data) return <p>Loading...</p>;
     return (
-        <>
-        <h1 style={styles}>{`${data.name} ${data.surname}`}</h1>
+        <div style={navaBarStyles}>
+        <h1 style={nameStyles}>{`${data.name} ${data.surname}`}</h1>
         <img src='/images/profilePhoto.png' alt={`${data.name} ${data.surname}`} style={imageStyles}/>
-        <h2>Aspiring Software Developer</h2>
+        <h2 style={{color: 'white'}}>Aspiring Software Developer</h2>
         <p style={descriptionStyles}>I'm a passionate and dedicated software developer with a strong desire to create innovative solutions. With a solid foundation in programming languages such as JavaScript, Python, and Java, I have experience in developing web applications, mobile apps, and software solutions. I am constantly learning and staying up-to-date with the latest technologies to enhance my skills and deliver high-quality work. I am eager to contribute my knowledge and creativity to projects that make a positive impact.</p>
         <p>Welcome to my portfolio</p>
         <p>I am {`${data.age} `} Years Old</p>
-        </>
+        </div>
     )
 };
 
