f46449118cf103cddd667f4fa9244f2ca995427e
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-04-23 11:30:46 +0200

    Update Skills and Add style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import NavBar from './navbar/names';
 import Footer from './navbar/footer';
-import Skills from './navbar/programmingskill';
 import WebDev from './navbar/webdevskills';
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Skills />
       <WebDev />
       <Footer />
     </div>
--- src/navbar/webdevskills.js
+++ src/navbar/webdevskills.js
@@ -1,19 +1,95 @@
 import data from "../data/data.json"; // adjust path to the component's locatio
 
 const WebDev =() => {
-const webDevSkills = data?.skills?.webDevelopment.javaScriptFrameWorks?.frontEnd ?? [];
+const theSkills = data?.skills?.programmingLanguages ?? [];
+const javaScriptfrontEndwebDevSkills = data?.skills?.webDevelopment.javaScriptFrameWorks?.frontEnd ?? [];
+const javaScriptbackEndwebDevSkills = data?.skills?.webDevelopment.javaScriptFrameWorks?.backEnd ?? [];
+const pythonFrontEndSkills = data?.skills?.webDevelopment.pythonFrameWorks ?? [];
+const javaBackEnd = data?.skills?.webDevelopment.javaFrameWorks ?? [];
+const databases = data?.skills?.database ?? [];
+const cloud = data?.skills?.cloudAndDevOps ?? [];
+const thetoolsAndPlatforms = data?.skills?.toolsAndPlatforms?.microSoft ?? [];
+const developmentTools = data?.skills?.toolsAndPlatforms?.devTools ?? [];
+const UI_and_UX = data?.skills?.toolsAndPlatforms?.UI_UX_Design ?? [];
 
-if (webDevSkills.length === 0) return <p>No web development skills available.</p>;
+if (
+	javaScriptfrontEndwebDevSkills.length === 0 &&
+	javaScriptbackEndwebDevSkills.length === 0 &&
+	pythonFrontEndSkills.length === 0 && 
+	javaBackEnd.length === 0 &&
+	databases.length === 0 &&
+	cloud.length === 0 &&
+	thetoolsAndPlatforms.length === 0 &&
+	developmentTools.length === 0 &&
+	UI_and_UX.length === 0 &&
+	theSkills.length === 0	
+)
+	return <p>No web development skills available.</p>;
+
 return (
-<div>
-<h2>Web Development Skills</h2>
-<ul>
-{webDevSkills.map((s, i) => (
-<li key={i}>{s}</li>
-))}
-</ul>
-</div>
-);
-};
+	<div>
+		<div style={{backgroundColor: 'grey', color: 'black', padding: '2px', borderRadius: '10px', marginBottom: '2px', marginTop: '2px', boxShadow: '20px 20px 20px rgba(0, 0, 0, 0.1)'}}>
+		<h2>Programming Skills</h2>
+		<dev>
+		{theSkills.map((s, i) => (
+		<li key={i}>{s}</li>
+		))}
+		</dev>
+	</div>
+	<div style={{backgroundColor: 'grey', color: 'black', padding: '2px', borderRadius: '10px', marginBottom: '2px', marginTop: '2px', boxShadow: '20px 20px 20px rgba(0, 0, 0, 0.1)'}}>
+		<h3>Web Development Skills</h3>
+		<h3>Front-End</h3>
+		<div>
+			<h4>JavaScript Front-End</h4>
+		{javaScriptfrontEndwebDevSkills.map((s, i) => (
+		<li key={i}>{s}</li>
+		))}
 
+		<h3>Back-End</h3>
+		<h4>Python Frameworks</h4>
+		{pythonFrontEndSkills.map((s, i) =>(
+		<li key={i}>{s}</li>
+		))}
+
+		<h4>JavaScript Back-End</h4>
+
+		{javaScriptbackEndwebDevSkills.map((s, i) => (
+		<li key={i}>{s}</li>
+		))}
+
+		<h4>Java Back-End</h4>
+		{javaBackEnd.map((s, i) => (
+			<li key={i}>{s}</li>
+		))}
+		</div>
+		<h4>Databases</h4>
+		{databases.map((s, i) => (
+			<li key={i}>{s}</li>
+		))}
+		</div>
+		<div style={{backgroundColor: 'grey', color: 'black', padding: '2px', borderRadius: '10px', marginBottom: '2px', marginTop: '2px', boxShadow: '20px 20px 20px rgba(0, 0, 0, 0.1)'}}>
+			<h3>Cloud and DevOps</h3>
+		{cloud.map((s, i) => (
+			<li key={i}>{s}</li>
+		))}
+
+		<h3>Tools and Platforms</h3>
+		<h4>Microsoft</h4>
+		{thetoolsAndPlatforms.map((s, i) => (
+			<li key={i}>{s}</li>
+		))}
+
+		<h4>Development Tools</h4>
+		{developmentTools.map((s, i) => (
+			<li key={i}>{s}</li>
+		))}
+
+		<h4>UI/UX Design</h4>
+		{UI_and_UX.map((s, i) => (
+			<li key={i}>{s}</li>
+			))}
+		</div>
+	</div>
+)
+};
 export default WebDev;
