bef4e46797fd90812911ea2e6e2ed445e31d95d8
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-05-23 00:51:16 +0200

    Reduce the fontSize of the footer content

--- src/navbar/footer.js
+++ src/navbar/footer.js
@@ -40,7 +40,7 @@ const Footer = () => {
                     <p>{data.address.city}, {data.address.province}, {data.address.country}, {data.address.code}</p>
                 </div>
                 <div style={footerStyles}>
-                    <p style={{ color: 'white' }}>Copyright &copy; 2026 Matsepe Kgodiso | Built With React</p>
+                    <p style={{ color: 'white', fontSize: "13px" }}>&copy; 2026 Matsepe Kgodiso | Built With React</p>
                 </div>
             </footer>
     );
