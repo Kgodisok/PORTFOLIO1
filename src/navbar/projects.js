bea3a63bbfac691aa4dc6f16c35389cbdab5137f
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-05-22 22:59:20 +0200

    Fix project's sentence

--- src/navbar/projects.js
+++ src/navbar/projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
             <h1>Projects</h1>
             <h2>Portmat – Accommodation Marketplace</h2>
             <p>Engineered Portmat, a scalable accommodation marketplace designed to simplify room discovery and property management with modern web technologies and responsive user experience.</p>
-            <p><strong>Tech stack</strong> (React, Next.js, Node.js, API, etc.)</p>
+            <p><strong>Tech stack</strong> (React, Next.js, Node.js, API, JSON)</p>
             <a href="https://portmat-accommodation.vercel.app"><button className="live-btn">Live Demo</button></a>
         </div>
     )
