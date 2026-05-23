2dd7a3d7124dd8b08228ce1371675aad9c6da4a8
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-05-23 13:02:40 +0200

    Add colors to my projects' tech-stack icons

--- src/navbar/projects.js
+++ src/navbar/projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
             <h1>Projects</h1>
             <h2>Portmat – Accommodation Marketplace</h2>
             <p>Engineered Portmat, a scalable accommodation marketplace designed to simplify room discovery and property management with modern web technologies and responsive user experience.</p>
-            <p className="skill-item-project"><strong>Tech Stack</strong> React <SiReact size={30}/>, Node <SiNodedotjs size={30}/>, Express <SiExpress size={30}/> & JSON <SiJson size={22}/></p>
+            <p className="skill-item-project"><strong>Tech Stack</strong> React <SiReact size={30} color="#61DBFB"/>, Node <SiNodedotjs size={30} color="#68A063"/>, Express <SiExpress size={30}/> & JSON <SiJson size={22}/></p>
             <a href="https://portmat-accommodation.vercel.app"><button className="live-btn">Live Demo</button></a>
         </div>
     )
