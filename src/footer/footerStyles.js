f8329b871f4fa0b70f24e6958494ffe6437306bb
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-05-29 14:28:10 +0200

    create a separate folder for footer

---
+++ src/footer/footerStyles.js
@@ -0,0 +1,13 @@
+export const iconStyles ={
+    display: 'flex',
+    gap: '8px',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+};
+
+export const footerStyles = { 
+    display: 'flex', 
+    justifyContent: 'center', 
+    backgroundColor: 'black', 
+    color: 'white',
+};
